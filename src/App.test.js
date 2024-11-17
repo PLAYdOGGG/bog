0e2c8200208d5b0af0c15fa61b05d6efaff33a62
MacBook <macbook@MacBooks-MacBook-Pro.local>
2024-11-17 09:25:59 -0800

    fix

--- src/App.test.js
+++ src/App.test.js
@@ -1,2 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+test('renders My own React App text', () => {
+  render(<App />);
+
+  const textElement = screen.getByText(/my react App/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders logo image', () => {
+  render(<App />);
+
+  const logoElement = screen.getByAltText(/logo/i);
+  expect(logoElement).toBeInTheDocument();
+});
